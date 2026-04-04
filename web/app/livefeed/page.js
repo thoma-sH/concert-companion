cb0c7a31ae280e397193569657229157cf96b063
7lrsergio <checojlr11@outlook.com>
2026-04-04 09:05:51 -0500

    9:05 2.0

--- web/app/livefeed/page.js
+++ web/app/livefeed/page.js
@@ -505,7 +505,7 @@ export default function ConcertCompanion() {
                   {[0, 1, 2, 3, 4, 5, 6, 7, 8].map((i) => (
                     <img
                       key={i}
-                      src={`/Gifs/gif${i}.gif`}
+                      src={`web/Gifs/gif${i}.gif`}
                       className="gif-option"
                       alt={`gif ${i}`}
                       onClick={() => postGif(`/Gifs/gif${i}.gif`)}
